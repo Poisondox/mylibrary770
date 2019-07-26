1147694504849b57fbab48b33858272ebff41054
anonymous <550572769@qq.com>
2019-07-26 11:57:17 +0800

    do execl to database.

--- src/dispatcher/rawdata_bus.py
+++ src/dispatcher/rawdata_bus.py
@@ -31,6 +31,9 @@ class RawDataBus:
         if not self.cursor:
             raise(NameError,'连接数据库失败')
             logging.error('连接数据库失败')
+            print("database link error")
+        else:
+            logging.info('连接数据库初始化完成')
     
     def __del__(self):
         self.conn.close()
@@ -46,8 +49,20 @@ class RawDataBus:
         self.conn.commit()
         logging.debug("数据写入数据库完成"+isbn)
         #this is test function
-        
-
+    
+    #批量导入
+    def AdapterExecltoDatabase(self,execl_path):
+        #构建原始数据
+        raw_data = BuildDatabaseRawData(execl_path)
+        #执行写入操作(批量写入)
+        logging.info("start to write to database")
+        self.cursor.executemany(self.insert_sql_temple,raw_data)
+        #执行保存
+        self.conn.commit()
+        logging.info("execl to database succeed,")
+    
+    
+    #关闭数据库连接
     def closeConn(self):
         self.conn.close()
 
@@ -74,17 +89,50 @@ def ISBNListToBookInformation(list_isbn):
     for index in range(0,isbn_list_length):
         #查找图书详情
         bookdetail = isbnEngine.get_information_with_ISBN(list_isbn[index])
+        if bookdetail == -1:
+            #无此图书信息(占位补充)
+            bookdetail = ['none','none','none','none','none']
         #构建图书详细信息
         bookdetaillist.append(bookdetail)
-        
     logging.info('book detail list build succeed.')
+    return bookdetaillist
     
+#构建输入数据库的基础数据
+def BuildDatabaseRawData(execl_filepath):
+        #原始数据列表
+        raw_data_list = []
+        #isbn清单
+        list_isbn = execlDataToList(execl_filepath)
+        #图书信息清单
+        book_details_list = ISBNListToBookInformation(list_isbn)
+        logging.info("build base information succeed.")
+        #
+        if len(list_isbn) != len(book_details_list):
+            logging.error('列表数据长度不符')
+            raise Exception('长度不符')
+        for index in range(0,len(list_isbn)):
+            #构建基础元组数据
+            tmpmeta = (list_isbn[index],book_details_list[index][0],book_details_list[index][1],
+                       book_details_list[index][2],book_details_list[index][3],
+                       book_details_list[index][4],'',datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            #显示基础信息
+            logging.info(tmpmeta.__str__())
+            #构建整体数据集
+            raw_data_list.append(tmpmeta)
     
+        return raw_data_list
+
+
+
+
+
 
 #test function
 if __name__ == '__main__':
-    list = execlDataToList("D:/rawdata/mylibrary/all_isbn.xlsx")
-    details = ISBNListToBookInformation(list)
+    #list = execlDataToList("D:/rawdata/mylibrary/apart_isbn.xlsx")
+    #details = ISBNListToBookInformation(list)
+    rb = RawDataBus()
+    rb.AdapterExecltoDatabase("D:/rawdata/mylibrary/apart_isbn.xlsx")
 
 
 
